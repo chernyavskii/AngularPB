a5fe2eadfdf65a268b2f49592fe27c6302e77801
m.chernyavskii <tyuoc@mail.ru>
2018-03-30 18:56:09 +0300

    agents module true settings module update

--- src/app/components/dashboard/agents/update-agent/update-agent.component.ts
+++ src/app/components/dashboard/agents/update-agent/update-agent.component.ts
@@ -26,7 +26,7 @@ export class UpdateAgentComponent implements OnChanges {
     });
   }
 
-  testFind() {
+  pushItem() {
     for (let i = 0; i < this.agents.length; i++) {
       this.items.push(this.fb.group({
         id: this.agents[i].id,
@@ -49,7 +49,6 @@ export class UpdateAgentComponent implements OnChanges {
   checkIdCurrent(id: number): boolean {
     for (let i = 0; i < this.changes.agents.previousValue.length; i++) {
       if (id === this.changes.agents.previousValue[i].id) {
-        console.log('DS: ');
         return true;
       }
     }
@@ -59,8 +58,6 @@ export class UpdateAgentComponent implements OnChanges {
   checkIdPrevious(id: number): boolean {
     for (let i = 0; i < this.changes.agents.currentValue.length; i++) {
       if (id === this.changes.agents.currentValue[i].id) {
-        console.log('UI: ');
-        console.log(this.changes.agents.currentValue[i].firstName);
         return true;
       }
     }
@@ -68,10 +65,9 @@ export class UpdateAgentComponent implements OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    console.log(changes);
     this.changes = changes;
     if (changes.agents.firstChange) {
-      this.testFind();
+      this.pushItem();
     } else {
       if (changes.agents.currentValue.length > changes.agents.previousValue.length) {
         for (let i = 0; i < changes.agents.currentValue.length; i++) {
@@ -97,90 +93,17 @@ export class UpdateAgentComponent implements OnChanges {
       } else {
         for (let i = 0; i < changes.agents.previousValue.length; i++) {
           const result = this.checkIdPrevious(changes.agents.previousValue[i].id);
-          console.log(result);
           if (!result) {
             for (let k = 0; k < this.items.length; k++) {
               if (this.items.at(k).value.id === changes.agents.previousValue[i].id) {
                 this.items.removeAt(k);
               }
-              /* this.items.removeAt(k);
-
-               console.log(this.items.at(k).value.firstName);
-
-
-               this.items.at(k).value.id
- */
             }
-            console.log('z');
           }
         }
       }
     }
   }
-
-  /*ngOnChanges(changes: SimpleChanges) {
-    if (changes.agents.firstChange) {
-      for (let i = 0; i < this.agents.length; i++) {
-        this.items.push(this.fb.group({
-          id: this.agents[i].id,
-          firstName: this.agents[i].firstName,
-          middleName: this.agents[i].middleName,
-          lastName: this.agents[i].lastName,
-          address: this.agents[i].address,
-          bank: this.agents[i].bank,
-          bik: this.agents[i].bik,
-          ks: this.agents[i].ks,
-          organization: this.agents[i].organization,
-          phone: this.agents[i].phone,
-          position: this.agents[i].position,
-          unp: this.agents[i].unp,
-          rs: this.agents[i].rs,
-        }));
-      }
-    }
-    if (!changes.agents.firstChange && changes.agents.currentValue.length === 0) {
-      for (let i = 0; i < this.items.length; i++) {
-        this.items.removeAt(i);
-      }
-    }
-
-    if (changes.agents.currentValue && changes.agents.previousValue) {
-      for (let i = 0; i < changes.agents.previousValue.length; i++) {
-        for (let k = 0; k < changes.agents.currentValue.length; k++) {
-          if (changes.agents.currentValue[k].id === changes.agents.previousValue[i].id) {
-            console.log(this.items.length);
-            for (let z = 0; z < this.items.length; z++) {
-              if (this.items.at(z).value.id === changes.agents.currentValue[k].id) {
-                console.log('qqq');
-                this.items.removeAt(z);
-              }
-            }
-
-          } else {
-            this.items.push(this.fb.group({
-              id: changes.agents.currentValue[k].id,
-              firstName: changes.agents.currentValue[k].firstName,
-              middleName: changes.agents.currentValue[k].middleName,
-              lastName: changes.agents.currentValue[k].lastName,
-              address: changes.agents.currentValue[k].address,
-              bank: changes.agents.currentValue[k].bank,
-              bik: changes.agents.currentValue[k].bik,
-              ks: changes.agents.currentValue[k].ks,
-              organization: changes.agents.currentValue[k].organization,
-              phone: changes.agents.currentValue[k].phone,
-              position: changes.agents.currentValue[k].position,
-              unp: changes.agents.currentValue[k].unp,
-              rs: changes.agents.currentValue[k].rs,
-            }));
-            console.log(changes.agents.currentValue[k]);
-          }
-        }
-      }
-    }
-    console.log(changes);
-
-  }*/
-
 
   get items(): FormArray {
     return this.updateAgentForm.get('items') as FormArray;
@@ -206,7 +129,6 @@ export class UpdateAgentComponent implements OnChanges {
     this.agentService.updateAgent(formValue.value.id, updateAgent)
       .then(data => {
         const array: Agent[] = [formValue.value];
-        console.log(array);
         this.onVoted.emit(array);
         this.onLoad = false;
       })
